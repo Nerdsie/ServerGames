1ee511cde3add9f8af098a5d3921d0b467a19037
Your Name <brenhein@brenhein.org>
2012-05-19 08:32:30 -0400

    Working chest reset code :)

--- ServerGames/src/me/NerdsWBNerds/ServerGames/Objects/Chests.java
+++ ServerGames/src/me/NerdsWBNerds/ServerGames/Objects/Chests.java
@@ -1,15 +1,19 @@
 package me.NerdsWBNerds.ServerGames.Objects;
 
+import me.NerdsWBNerds.ServerGames.ServerGames;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.WorldType;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Chests created by Brenhein
@@ -17,17 +21,27 @@ import java.util.ArrayList;
 public class Chests {
 
 
-    // WIP -bren
-    // DO NOT USE!!!!!1111111oneoneone!!111shift!!!111
-    public void resetChests(Plugin plugin){
+    // might not work
+    public static void resetChests(ServerGames plugin, Location corn){
+        // fishing rod, bow, arrow, diamond, wood sword, compass, iron sword, stone sword, gold sword, all armour
+        int[] items = { 346, 261, 262, 260, 264, 268, 345, 267, 272, 283, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 317 };
+        ArrayList<ItemStack> arrayList = new ArrayList<ItemStack>()
         for(World f : Bukkit.getWorlds()){
             if(f.getWorldType() == WorldType.NORMAL){
-                //f.getEntities()
+                BlockState[] blockState = f.getChunkAt(corn).getTileEntities();
+                for(BlockState tileEntity : blockState){
+                    Block chestBlock = tileEntity.getBlock();
+                    Random rnd = new Random();
+                    for(int i = 0; i < 7; i++){
+                        arrayList.add(new ItemStack(items[rnd.nextInt(items.length)], 1));
+                    }
+                    setChest(chestBlock, arrayList);
+                }
             }
         }
     }
 
-    public void setChest(Block chest, ArrayList<ItemStack> items){
+    public static void setChest(Block chest, ArrayList<ItemStack> items){
         Chest chestTileEntity = null;
         try{ chestTileEntity = (Chest) chest; } catch(Exception e){  } // yeah yeah let java scream at us
         Inventory chestInv = chestTileEntity.getBlockInventory();
