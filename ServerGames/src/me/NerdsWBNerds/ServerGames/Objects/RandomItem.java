b34d3e04fb3026b0272ddb8edce529a816131d4f
NerdsWBNerds <richcoll97@gmail.com>
2012-05-19 15:13:00 -0500

    Value changes

--- ServerGames/src/me/NerdsWBNerds/ServerGames/Objects/RandomItem.java
+++ ServerGames/src/me/NerdsWBNerds/ServerGames/Objects/RandomItem.java
@@ -1,17 +1,21 @@
 package me.NerdsWBNerds.ServerGames.Objects;
 
+import org.bukkit.Material;
+
 public class RandomItem {
-	public static RandomItem air = new RandomItem(0, 30);
+	public static RandomItem air = new RandomItem(0, 200);
 	public static RandomItem stonesword = new RandomItem(272, 5);
-	public static RandomItem woodsword = new RandomItem(268, 10);
-	public static RandomItem leatherheml = new RandomItem(298, 8);
-	public static RandomItem leatherchest = new RandomItem(299, 8);
-	public static RandomItem leatherpants = new RandomItem(300, 8);
-	public static RandomItem leathershoes = new RandomItem(301, 8);
+	public static RandomItem woodsword = new RandomItem(268, 7);
+	public static RandomItem leatherheml = new RandomItem(298, 6);
+	public static RandomItem leatherchest = new RandomItem(299, 6);
+	public static RandomItem leatherpants = new RandomItem(300, 6);
+	public static RandomItem leathershoes = new RandomItem(301, 6);
 	public static RandomItem bow = new RandomItem(261, 5);
-	public static RandomItem arrow = new RandomItem(262, 10);
+	public static RandomItem arrow = new RandomItem(262, 6);
 	public static RandomItem apple = new RandomItem(260, 4);
 	public static RandomItem compass = new RandomItem(345, 4);
+	public static RandomItem iron = new RandomItem(Material.IRON_INGOT.getId(), 4);
+	public static RandomItem diamond = new RandomItem(Material.DIAMOND.getId(), 2);
 	
 	public int id, chance;
 	
